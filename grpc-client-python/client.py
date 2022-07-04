4d8d4cbd256b6f88d158292d030b5f0e9787426c
Pouria <pouriaroostaee.ce@gmail.com>
2022-07-03 23:10:14 -0400

    Initialize commit

---
+++ grpc-client-python/client.py
@@ -0,0 +1,20 @@
+import grpc
+
+## Import generated classes
+
+
+# Open a gRPC channel
+channel = #
+
+
+# Create a stub for gRPC client
+stub = #
+
+# Create a valid request
+request = #
+
+# Make the call
+response = #
+
+# Output response
+print(#)
